65efd54b6733e22e1143e7a914de5208c4dbb66c
Inseo Hwang <sjsjsj1246@gmail.com>
2021-09-14 04:21:08 +0900

    react-navigation 추가

---
+++ navigation/index.tsx
@@ -0,0 +1,17 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { ColorSchemeName } from "react-native";
+
+export default function Navigation({
+  colorScheme,
+}: {
+  colorScheme: ColorSchemeName;
+}) {
+  return (
+    <NavigationContainer
+      linking={LinkingConfiguration}
+      theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+    >
+      <RootNavigator />
+    </NavigationContainer>
+  );
+}
