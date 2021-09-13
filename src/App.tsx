3d10bb097973bfacd1ead9b7a6cd852e9b5f25ef
Inseo Hwang <sjsjsj1246@gmail.com>
2021-09-14 05:02:49 +0900

    Feat: Bottom Tabs, Modal

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { registerRootComponent } from "expo";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { useColorScheme } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import useCachedResources from "./hooks/useCachedResources";
+import Navigation from "./navigation";
+
+const App = () => {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <Navigation colorScheme={colorScheme} />
+        <StatusBar />
+      </SafeAreaProvider>
+    );
+  }
+};
+
+export default registerRootComponent(App);
